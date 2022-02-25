48905edb680095d273bcfae20d92b9abbe1384e7
mery.acevedo <mery.acevedo@catolica.edu.sv>
2022-02-24 23:26:37 -0600

    agregacion columnas faltantes

--- models/equipos.cs
+++ models/equipos.cs
@@ -13,6 +13,19 @@ namespace equiposApi.models
         public string nombre { get; set; }
         public string descripcion { get; set; }
 
+        public int tipo_equipo_id { get; set; }
+
+        public int marca_id { get; set; }
+
+        public string modelo { get; set; }
+
+        public int anio_compra { get; set; }
+
+        public decimal costo { get; set; }
+        public int vida_util { get; set; }
+        public int estado_equipo_id { get; set; }
+        public char estado { get; set; }
+
 
     }
 }
